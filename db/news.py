b3938f153286cd8df445b6a893b24ad7c029e0f5
Jernej Virag <jernej@virag.si>
2013-08-02 16:55:00 +0200

    Fixed bugs when inserting news into database

--- db/news.py
+++ db/news.py
@@ -40,11 +40,20 @@ def store_news(news):
     for news_item in news:
         if news_item["id"] in existing_ids:
             continue
+
+        existing_ids.add(news_item["id"])
+
         db_item = NewsItem(id=news_item["id"], title=news_item["title"],
                            source=news_item["source"], source_url=news_item["source_url"],
                            published=news_item["published"], content=news_item["text"], author=news_item["author"])
-        db_session.merge(db_item)
+        db_session.add(db_item)
         count += 1
 
     db_session.commit()
     print "Added ", count,  " new items."
+
+def get_news():
+    db_engine = create_engine(settings.DB_CONNECTION_STRING)
+    Session = sessionmaker(bind=db_engine)
+    db_session = Session()
+    return db_session.query(NewsItem).yield_per(50)
