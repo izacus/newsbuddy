fe3c3c84b62b4a85a1a659db5dfe9a197dde30fb
Jernej Virag <jernej@virag.si>
2013-08-02 17:08:20 +0200

    Implemented script to import existing database to solr

---
+++ db_to_solr.py
@@ -0,0 +1,23 @@
+import logging
+import settings
+import db.news
+import pytz
+from solr import solr
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    
+    solr_int = solr.SolrInterface(settings.SOLR_ENDPOINT_URLS, settings.SOLR_DEFAULT_ENDPOINT)
+    # Now iterate over news
+    docs = []
+    for news_item in db.news.get_news():
+        doc = { "id" : news_item.id, "title" : news_item.title,
+                "source" : news_item.source, "language" : "si",
+                "source_url" : news_item.source_url, "content" : news_item.content,
+                "published" : solr.to_solr_date(news_item.published.replace(tzinfo=pytz.utc)) }    
+        docs.append(doc)
+
+    solr_int.add(docs)
+    solr_int.commit()
+
+    print "Dispached " + str(len(docs)) + " documents to solr. "
