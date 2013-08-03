439a606bf635c93c6962307e114c31f516896e75
Jernej Virag <jernej@virag.si>
2013-08-03 19:19:42 +0200

    Author fixes, more UI

--- ui/js/controller.js
+++ ui/js/controller.js
@@ -1,6 +1,10 @@
 function SearchResultsController($scope, $http) {
-    $http.get('/news/query/?q=janez').success(function data(data) {
-        $scope.results = data["results"];
-        console.info(data);
-    });
+
+    $scope.search = function() {
+        console.info($scope.query)
+        $http.get('/news/query/?q=' + $scope.query).success(function data(data) {
+            $scope.results = data["results"];
+            console.info(data);
+        });
+    }
 }
