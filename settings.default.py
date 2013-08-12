e6ae92be8158cedf3bc87d000bab8378c2372a8d
Jernej Virag <jernej@virag.si>
2013-08-12 19:12:20 +0000

    Added Sentry support for the UI part

--- settings.default.py
+++ settings.default.py
@@ -1,5 +1,5 @@
 DB_CONNECTION_STRING = "sqlite:///articles.db"
-
+SENTRY_CONNECTION_STRING = None
 # Dictionary of endpoints; key is a language code, value is URL
 SOLR_ENDPOINT_URLS = { "si" : "http://localhost:8983/solr/news/"}
 # Default endpoint to dispatch documents in languages without dedicated endpoint
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
 import api.query
 from cornice import register_service_views
 from pyramid.config import Configurator
+import settings
 
 config = Configurator()
 config.include('cornice')
 register_service_views(config, api.query.news_query)
 config.add_static_view('', 'ui')
 application = config.make_wsgi_app()
+
+if settings.SENTRY_CONNECTION_STRING is not None:
+   from raven import Client
+   from raven.middleware import Sentry
+   client = Client(settings.SENTRY_CONNECTION_STRING)
+   application = Sentry(application, client=client)
