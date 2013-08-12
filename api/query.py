12e76536b1eef63fd9a5fe6378bbe49f6ded87be
Jernej Virag <jernej@virag.si>
2013-08-12 15:24:01 +0200

    Added pagination to API

--- api/query.py
+++ api/query.py
@@ -1,18 +1,24 @@
 from cornice import Service
 import settings
+import math
 from pysolarized import solr
 from pysolarized import from_solr_date
 
 news_query = Service(name="news_query", path="/news/query/", description="Returns news matching the query")
+
+PAGE_SIZE = 30
 
 @news_query.get()
 def get_news(request):
     if "q" not in request.GET:
         return { "error" : "Missing q query parameter." }
 
+    start_index = 0
+    if "page" in request.GET:
+        start_index = int(request.GET["page"]) * PAGE_SIZE 
 
     solr_int = solr.Solr(settings.SOLR_ENDPOINT_URLS, settings.SOLR_DEFAULT_ENDPOINT)
-    results = solr_int.query(request.GET["q"], sort=["published desc"])
+    results = solr_int.query(request.GET["q"], sort=["published desc"], start=start_index, rows=PAGE_SIZE)
 
     if results is None:
         return { "error" : "Failed to connect to news search server." }
@@ -47,5 +53,11 @@ def get_news(request):
             r["facets"]["published"] = published_facets
         if "source" in results.facets:
             r["facets"]["source"] = results.facets["source"]
+
+    # Calculate pagination information
+    current_page = results.start_index / PAGE_SIZE
+    total_pages = int(math.ceil(results.results_count / float(PAGE_SIZE)))
+
+    r["pagination"] = { "current" : current_page, "total" : total_pages, "results" : results.results_count }
 
     return r
