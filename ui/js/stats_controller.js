8b76581e5c30b0ce14e40df139d3a6c2a46a80d2
Jernej Virag <jernej@virag.si>
2013-08-15 17:27:40 +0200

    Added news-by-category chart

--- ui/js/stats_controller.js
+++ ui/js/stats_controller.js
@@ -1,9 +1,37 @@
 var newsBuddy = angular.module('NewsBuddy', []);
+google.load("visualization", "1", { packages: ["corechart"]});
+
 
 newsBuddy.controller('StatsController', function($scope, $http) {
     $scope.stats = { "total_news" : 0, "news_today": 0 };
 
     $http.get('/news/stats/').success(function data(data) {
         $scope.stats = data;
+
+
+        var sources = data["total_by_source"];
+        var labels = [ null ]
+        var data = [ null ]
+
+        for (var item in sources) {
+            if (sources.hasOwnProperty(item)) {
+                labels.push(item);
+                data.push(sources[item]);
+            }
+        }
+
+
+        // Draw data chart
+        var data = google.visualization.arrayToDataTable([
+            labels, data
+        ]);
+
+        var options = {
+            hAxis: { title: "Novic" },
+            tooltip: { showColorCode: true }
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('news-by-source'));
+        chart.draw(data, options);
     });
 });
