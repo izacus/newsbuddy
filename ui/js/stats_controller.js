c6b330615fabc6990343173cbf7415c7d43b86d2
Jernej Virag <jernej@virag.si>
2013-08-15 16:54:33 +0200

    Added basic stats

--- ui/js/stats_controller.js
+++ ui/js/stats_controller.js
@@ -1,3 +1,9 @@
-/**
- * Created by jernej on 8/15/13.
- */
+var newsBuddy = angular.module('NewsBuddy', []);
+
+newsBuddy.controller('StatsController', function($scope, $http) {
+    $scope.stats = { "total_news" : 0, "news_today": 0 };
+
+    $http.get('/news/stats/').success(function data(data) {
+        $scope.stats = data;
+    });
+});
