cffe549b5a015914a35fd9c2ed62b6433a1d7cfb
Jernej Virag <jernej@virag.si>
2013-08-27 17:14:22 +0200

    Added related news search

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,9 @@
-from api import query, stats
+from api import query, stats, related
 from cornice import register_service_views
 
 
 def register(config):
     register_service_views(config, query.news_query)
     register_service_views(config, query.latest)
+    register_service_views(config, related.related_query)
     register_service_views(config, stats.stats_service)
---
+++ api/related.py
@@ -0,0 +1,44 @@
+from cornice import Service
+from pysolarized import solr, from_solr_date
+import settings
+
+related_query = Service(name="related", path="/news/related/", description="Returns related news to passed result")
+
+@related_query.get()
+def get_related(request):
+
+    if "id" not in request.GET:
+        return {"error": "missing id parameter"}
+
+    id = request.GET["id"]
+    solr_int = solr.Solr(settings.SOLR_ENDPOINT_URLS, settings.SOLR_DEFAULT_ENDPOINT)
+    results = solr_int.more_like_this("id:%s" % id, ["title", "content"], ["id",
+                                                                           "title",
+                                                                           "published",
+                                                                           "source",
+                                                                           "source_url",
+                                                                           "author",
+                                                                           "score"])
+
+    if results is None:
+        return {u"error": u"Failed to connect to news search server."}
+
+    documents = []
+    for doc in results.documents:
+        document = {u"id": doc["id"],
+                    u"published": str(from_solr_date(doc["published"]).isoformat()) + "Z",
+                    u"link": doc["source_url"],
+                    u"source": doc["source"]}
+
+        if u"author" in doc and doc[u"author"] is not None:
+            document[u"author"] = doc["author"]
+        document[u"title"] = doc["title"]
+
+        if u"score" in doc:
+            document[u"score"] = doc["score"]
+
+        documents.append(document)
+
+    r = {u"results": documents}
+    return r
+
