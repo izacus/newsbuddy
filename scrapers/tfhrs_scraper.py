43e95d46aafe57e9d2a8ff1004152dbd99056ea1
Jernej Virag <jernej@virag.si>
2013-08-27 10:33:26 +0200

    Fixed 24hrs scraper

--- scrapers/tfhrs_scraper.py
+++ scrapers/tfhrs_scraper.py
@@ -8,7 +8,7 @@ logger = logging.getLogger("scraper.24ur")
 
 class TwentyFourHrsScraper(object):
 
-    TFH_RSS_URL = "http://rss.24ur.com/"
+    TFH_RSS_URL = "http://www.24ur.com/rss/"
 
     def get_news(self, existing_ids=None):
         news = []
@@ -43,27 +43,26 @@ class TwentyFourHrsScraper(object):
     def get_article(self, link):
         logger.debug("Grabbing article %s", link)
 
-        article_html = get_article(link)
+        article_html = get_article(link.replace("24ur.com", "www.24ur.com"))
         result = {}
 
         article = bs4.BeautifulSoup(article_html)
 
         summary = article.find(class_="summary")
         if summary is not None:
-            result["subtitles"] = summary.string.strip()
+            result["subtitles"] = summary.text.strip()
 
         # Try to find author
         author_container = article.find(class_="containerLeftSide")
         if author_container is not None:
-            container_text = author_container.string.strip()
+            container_text = author_container.text.strip()
             author = container_text[container_text.rfind('|'):]
             result["author"] = author
         else:
             result["author"] = None
 
         text = article.find(id="content")
-        result["text"] = u"\n".join(text.stripped_strings)
+        result["text"] = text.text.strip()
         if u"Preverite vpisani naslov ali uporabite možnost iskanja po naših straneh." in result["text"]:
             return None
-
         return result
