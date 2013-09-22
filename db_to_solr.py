553c4fa3b9b24a488e8389d1bfb3a110a651ad41
Jernej Virag <jernej@virag.si>
2013-09-22 20:49:05 +0200

    Added delete all for solr reimport

--- db_to_solr.py
+++ db_to_solr.py
@@ -9,17 +9,18 @@ if __name__ == "__main__":
     logging.basicConfig()
     
     solr_int = solr.Solr(settings.SOLR_ENDPOINT_URLS, settings.SOLR_DEFAULT_ENDPOINT)
+    solr_int.deleteAll()
     # Now iterate over news
     docs = []
     count = 0
     for news_item in db.news.get_news():
-        doc = { "id" : news_item.id, "title" : news_item.title,
-                "source" : news_item.source, "language" : "si",
-                "source_url" : news_item.source_url, "content" : news_item.content,
-                "published" : to_solr_date(news_item.published.replace(tzinfo=pytz.utc)) }  
-        
+        doc = {u"id": news_item.id, u"title": news_item.title,
+               u"source": news_item.source, u"language": u"si",
+               u"source_url": news_item.source_url, u"content": news_item.content,
+               u"published": to_solr_date(news_item.published.replace(tzinfo=pytz.utc))}
+
         if news_item.author is not None:
-            doc["author"] = news_item.author
+            doc[u"author"] = news_item.author
 
         solr_int.add(doc)
         count += 1
