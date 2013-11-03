80138b5339865b0833477f7e358a0fdd5d2658a2
Jernej Virag <jernej@virag.si>
2013-11-03 13:48:44 +0100

    Added alembic for migrations

---
+++ alembic/versions/3095c2d804d_defaut.py
@@ -0,0 +1,22 @@
+"""Defaut
+
+Revision ID: 3095c2d804d
+Revises: None
+Create Date: 2013-11-03 13:48:04.037173
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3095c2d804d'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
