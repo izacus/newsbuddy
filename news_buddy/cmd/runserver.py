16508d3bdce9190819df24d2ee7b50043bcba820
Jernej Virag <jernej@virag.si>
2014-06-21 17:00:53 +0200

    Fixed wsgi application

--- news_buddy/cmd/runserver.py
+++ news_buddy/cmd/runserver.py
@@ -1,23 +1,14 @@
 #from db.cache import get_cache
+from db.cache import get_cache
 from news_buddy import app
-import news_buddy.api.v1
+
 
 def runserver(host, port):
     app.run(host, port, debug=True)
 
-    # logging.basicConfig(level=logging.DEBUG)
-    # config = Configurator()
-    # config.include('cornice')
-    # api.register(config)
-    # config.add_static_view('', '../../ui')
-    # config.add_renderer('atom', AtomRenderer)
-    # app = config.make_wsgi_app()
-    # server = make_server(host, port, app)
-    # server.serve_forever()
-
     # Clear memcached cache on startup
-    #cache = get_cache()
-    #cache.invalidate(True)
+    cache = get_cache()
+    cache.invalidate(True)
 
 
 @app.route("/")
--- news_buddy/wsgi.py
+++ news_buddy/wsgi.py
@@ -1,17 +1,9 @@
-import api
-from api.v1.query import AtomRenderer
+from news_buddy import app
 import logging
-from pyramid.config import Configurator
 from db.cache import get_cache
 import settings
 
-config = Configurator()
-
-config.include('cornice')
-api.register(config)
-config.add_static_view('', 'ui')
-config.add_renderer('atom', AtomRenderer)
-application = config.make_wsgi_app()
+application = app
 
 # Clear memcached cache on startup
 cache = get_cache()
