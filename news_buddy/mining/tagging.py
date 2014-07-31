d97dcef9ed5fcdba895ecee15bf81a4f0788bc7d
Jernej Virag <jernej@virag.si>
2014-07-31 11:25:36 +0200

    More performance fixes for tagging

--- news_buddy/mining/tagging.py
+++ news_buddy/mining/tagging.py
@@ -4,7 +4,7 @@ from lemmagen import lemmatizer
 import db
 import db.news
 from mining.entity_extractor import EntityExtractor
-from sqlalchemy.orm import load_only
+from sqlalchemy.orm import load_only, Load, defer
 from sqlalchemy.orm.exc import NoResultFound
 from db.tags import Tag     # Must be imported for NewsItem reverse mapping
 
@@ -13,12 +13,17 @@ logger = logging.getLogger("mining.newstagger")
 tagger = EntityExtractor()
 lem = lemmatizer.Lemmatizer()
 
+
 def tag_article(article_id):
     logger.info("Tagging %s..." % str(article_id))
 
     s = db.get_db_session()
+    _tag_article_inner(s, article_id)
+    s.commit()
 
-    article = s.query(db.news.NewsItem).options(load_only('id', 'title', 'content')).filter_by(id=article_id).one()
+
+def _tag_article_inner(s, article_id):
+    article = s.query(db.news.NewsItem).options(load_only('id', 'title', 'content')).get(article_id)
     s.add(article)
 
     article_tags = tagger.tag(" ".join([article.title, article.content]))
@@ -29,22 +34,17 @@ def tag_article(article_id):
     article.tags = []
 
     # Counter is here to curb memory usage
-    counter = 0
     for tag_name, tag_type in article_tags:
         news_tag = lem.lemmatize(tag_name).strip()
         try:
-            tag = s.query(db.tags.Tag).filter_by(tag_name=news_tag).options(FromCache("tags"), load_only('id')).one()
+            tag = s.query(db.tags.Tag).filter_by(tag_name=news_tag).options(FromCache("tags")).one()
         except NoResultFound:
             tag = db.tags.Tag(tag_name=news_tag, tag_type=tag_type)
-        tag.news_items.append(article)
-        s.add(tag)
+        article.tags.append(tag)
 
-        counter += 1
-        if counter > 10:
-            s.commit()
-            s.flush()
-            s = db.get_db_session()
-            s.add(article)
-
-    s.commit()
-    logger.info("Found %d tags." % len(article.tags))
+        #counter += 1
+        #if counter > 10:
+        #    s.commit()
+        #    s.flush()
+        #    s = db.get_db_session()
+        #    s.add(article)
