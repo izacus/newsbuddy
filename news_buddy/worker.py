16b8d8a5e8df97eb7499cba07fe61f062ba38660
Jernej Virag <jernej@virag.si>
2014-08-05 09:35:13 +0200

    Added Sentry logging to workers

--- news_buddy/worker.py
+++ news_buddy/worker.py
@@ -4,7 +4,18 @@ This is a RQ worker used for async task processing
 """
 
 import sys
+import settings
 from rq import Connection, Queue, Worker
+
+
+if settings.SENTRY_CONNECTION_STRING is not None:
+    from raven import Client
+    from raven.handlers.logging import SentryHandler
+    from raven.conf import setup_logging
+    logging.basicConfig(level=logging.WARN)
+    client = Client(settings.SENTRY_CONNECTION_STRING)
+    handler = SentryHandler(client)
+    setup_logging(handler)
 
 # Memory profiling
 class ThreadWorker(Worker):
